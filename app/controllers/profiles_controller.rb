9f5e91d9daced125d510efe9fa857fa854a7def5
Aurelien Dobbels <adobbels@gmail.com>
2018-01-05 10:48:16 +0100

    new profile form done

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,19 @@
 class ProfilesController < ApplicationController
   def new
+    @profile = Profile.new
+  end
+
+  def create
+    fail
+    @profile = Profile.new(profile_params)
+    @profile.user = current_user
+    if @profile.save
+      flash[:notice] = 'Profile was successfully created.'
+      redirect_to profile_path(@profile)
+    else
+      flash[:notice] = 'Your profile has not been created, please update your profile later.'
+      redirect_to root_path
+    end
   end
 
   def show
