e2bd3f9b4962de37f54dc780e3032a118647f773
Antoine STAUMONT <staumont.antoine@me.com>
2018-01-05 15:16:51 +0100

    articles controller et view ok

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,27 +1,44 @@
 class ArticlesController < ApplicationController
+  def index
+    @articles =Article.all
+  end
+
   def new
     @article = Article.new
+    @profile = current_user.profile
+     # @profile = current_user.profile
   end
 
   def create
     @article = Article.new(article_params)
+    @profile = current_user.profile
+    @article.profile = @profile
 
     if @article.save
-      # redirect_to
+    redirect_to root_path
     else
       render :new
     end
   end
 
+  def show
+    @article = Article.find(params[:profile_id])
+  end
+
 
   def edit
-    @article = Article.find(params[:id])
+    @article = Article.find(params[:profile_id])
+    @profile = current_user.profile
+
+    # @profile = current_user.profile
   end
 
   def update
-    @article = Article.find(params[:id])
+    @article = Article.find(params[:profile_id])
+    @profile = current_user.profile
+    @article.profile = @profile
     @article.update(article_params)
-    # redirect_to house_path(@house)
+    redirect_to root_path
   end
 
   private
