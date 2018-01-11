33d6be0af92b4c1e5faed0b0dc925882bfadd741
Aurelien Dobbels <adobbels@gmail.com>
2018-01-11 09:43:08 +0100

    coodinates added to profiles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180109100251) do
+ActiveRecord::Schema.define(version: 20180111084210) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,6 +64,8 @@ ActiveRecord::Schema.define(version: 20180109100251) do
     t.bigint "user_id"
     t.text "biographie"
     t.string "formation"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
