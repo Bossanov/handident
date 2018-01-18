84351bf569d3270fc79c9544244d85aaf0169b8e
Antoine STAUMONT <staumont.antoine@me.com>
2018-01-18 12:04:45 +0100

    view controller donation ok, ajout colonne moyen

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -1,4 +1,29 @@
 class DonationsController < ApplicationController
   def new
+    @donation = Donation.new
+    @profile = current_user.profile
   end
+
+  def create
+    @donation = Donation.new(donation_params)
+    @profile = current_user.profile
+    @donation.profile = @profile
+
+    if @donation.save
+      redirect_to root_path
+      flash[:notice] = 'Merci !'
+
+    else
+      render :new
+      flash[:notice] = 'Une erreur est survenue, veuillez recommencer ...'
+
+    end
+  end
+
+  private
+
+  def donation_params
+    params.require(:donation).permit(:value, :moyen)
+  end
+
 end
