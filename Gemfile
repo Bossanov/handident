21ff94fdadeeb6690c4b31ffa4032bc642b5c978
Aurelien Dobbels <adobbels@gmail.com>
2018-01-18 10:49:30 +0100

    contact form done

--- Gemfile
+++ Gemfile
@@ -20,6 +20,7 @@ gem "geocoder"
 gem "coffee-rails"
 gem "gmaps4rails"
 gem 'forest_liana'
+gem 'mail_form'
 source 'https://rails-assets.org' do
   gem "rails-assets-underscore"
 end
--- config/routes.rb
+++ config/routes.rb
@@ -18,5 +18,8 @@ Rails.application.routes.draw do
   get 'pages/test'
   get 'pages/all_articles_categories'
 
+  match '/contacts',     to: 'contacts#new', via: 'get'
+  resources "contacts", only: [:new, :create]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
