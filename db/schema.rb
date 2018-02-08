246292d16790e17463be4a7bbdaba5bd0c7f69bc
Antoine STAUMONT <staumont.antoine@me.com>
2018-02-08 21:51:49 +0100

    fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180118153647) do
+ActiveRecord::Schema.define(version: 20180208150230) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20180118153647) do
     t.datetime "updated_at", null: false
     t.bigint "profile_id"
     t.string "status"
+    t.string "theme"
     t.index ["profile_id"], name: "index_articles_on_profile_id"
   end
 
